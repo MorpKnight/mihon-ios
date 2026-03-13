8deb3586ef2d72ea9a9303c5ccded5c9f188824f
MorpKnight <christoffelsihombing@gmail.com>
2026-03-13 20:47:54 +0700

    Initial Commit

---
+++ Mihon IOS/Mihon_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mihon_IOSApp.swift
+//  Mihon IOS
+//
+//  Created by Giovan Christoffel Sihombing on 13/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Mihon_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
